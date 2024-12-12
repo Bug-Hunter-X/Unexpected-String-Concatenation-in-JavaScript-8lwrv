5ffb722f18778b6664755d68a0004536e671983b
BugHunter-X <askzenai@gmail.com>
2024-12-12 06:18:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+  return Number(a) + Number(b);
+}
+
+console.log(add(1, '2')); // Output: 3 (numerical addition)
+console.log(add(1, 2)); // Output: 3 (numerical addition)
+console.log(add('1', '2')); // Output: 3 (numerical addition) 
+console.log(add('1', 2)); // Output: 3 (numerical addition) 
